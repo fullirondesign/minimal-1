a82131065c28797dce1ad538c8c68b8588a52baf
full_iron <ilyafefelov@gmail.com>
2019-03-20 17:47:00 +0200

    refactor - sliderchange function 2

--- src/components/Showcase2.js
+++ src/components/Showcase2.js
@@ -3,24 +3,7 @@ import changeSliderPage from "./functions/changeSliderPage"
 
 const Slider = () => {
   const [sliderFlag, handlePromotion] = useState("1")
-  
-  // const changeSliderPage = (tag, e) => {
-  //   e.preventDefault()
-  //   console.log(
-  //     "The slider link was clicked.",
-  //     `was: ${sliderFlag}, became: ${tag}`
-  //   )
-
-  //   if (sliderFlag !== tag) {
-  //     console.log("change")
-  //     handlePromotion(tag)
-  //   } else {
-  //     console.log("no-change")
-  //   }
-  // }
-
-  // changeSliderPage(sliderFlag, handlePromotion, tag, e)
-
+ 
   return (
     <div className="section promotion promotion-2">
       <div className="promotion-2-nav">
--- src/components/functions/changeSliderPage.js
+++ src/components/functions/changeSliderPage.js
@@ -4,16 +4,19 @@
 // const [sliderFlag, handlePromotion] = useState("1")
 
 export default function changeSliderPage(sliderFlag, handlePromotion, tag, e) {
-    e.preventDefault();
-    console.log(
-        "The slider link was clicked.",
-        `was: ${sliderFlag}, became: ${tag}`
-    );
+  e.preventDefault();
+  console.log(
+    "The slider link was clicked.",
+    `It was: ${sliderFlag}, became: ${tag}`
+  );
 
-    if (sliderFlag !== tag) {
-        console.log("change");
-        handlePromotion(tag);
-    } else {
-        console.log("no-change");
+  if (sliderFlag !== tag) {
+    console.log("change");
+    handlePromotion(tag); //changing state with imported function from props 
+  } else
+    try {
+      console.log("no-change"); // no change indicator
+    } catch (e) {
+      e.console.error(); //error 
     }
-};
+}
