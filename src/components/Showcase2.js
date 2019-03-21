2153cd6eaad857b740c8315f51559226f6202982
full_iron <ilyafefelov@gmail.com>
2019-03-21 13:28:18 +0200

    Update Showcase2.js

--- src/components/Showcase2.js
+++ src/components/Showcase2.js
@@ -1,48 +1,54 @@
-import React, { useState } from "react"
-import changeSliderPage from "./functions/changeSliderPage"
+import React, { useState } from "react";
+import changeSliderPage from "./functions/changeSliderPage";
 
-const SliderPage = (sliderFlag) => (
+// data
+const sliderPages = ["1", "2", "3", "4", "5", "6"];
+
+// template TODO: content rendering
+const page = flag => (
   <div className="promotion-container">
     <div className="promotion-container-inner">
       <div className="circle" />
-      <div className="circle" />
-      <div className="circle" />
-      <img src="..\icons\iphone.png" alt={sliderFlag} />
     </div>
-    {/* <vd /> */}
-    <div className="promotion-container-inner promotion-text">
-      {sliderFlag}
-    </div>
-  </div>)
+    <div className="promotion-container-inner promotion-text">{flag}</div>
+  </div>
+);
 
-const button = (sliderFlag, handlePromotion, tag) => (
+// conditional rendering of the sliderPage
+const RenderSliderPage = sliderFlag => (
+  <>
+    {sliderFlag === "1" && page(1)}
+    {sliderFlag === "2" && page(2)}
+    {sliderFlag === "3" && page(3)}
+    {sliderFlag === "4" && page(4)}
+    {sliderFlag === "5" && page(5)}
+    {sliderFlag === "6" && page(6)}
+  </>
+);
+
+const buttonToChangeSliderPage = (sliderFlag, handlePromotion, tag) => (
   <div
     className="promotion-2-nav-link"
     id={tag}
     onClick={e => changeSliderPage(sliderFlag, handlePromotion, tag, e)}
   >
-    <img src="" alt="" />{tag}
+    <img src="" alt={""} />
+    {tag}
   </div>
 )
 
-
-const sliderPages = [
-  '1', '2', '3', '4', '5', '6',
-]
-
 const Slider = () => {
-  const [sliderFlag, handlePromotion] = useState("1")
-
+  const [sliderFlag, handlePromotion] = useState("1");
   return (
     <div className="section promotion promotion-2">
       <div className="promotion-2-nav">
         {sliderPages.map(page =>
-          button(sliderFlag, handlePromotion, page)
+          buttonToChangeSliderPage(sliderFlag, handlePromotion, page)
         )}
       </div>
-      <div>
-        {(sliderFlag === "1" || "2" || "3" || "4" || "5" || "6") && SliderPage(sliderFlag)}
-      </div>
+        {sliderFlag.includes("1", "2", "3", "4", "5", "6") &&
+          RenderSliderPage(sliderFlag)
+        }
     </div>
   );
 };
