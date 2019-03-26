c656244517e86ea7cee9814cec49d0bd7d02b3b4
full_iron <ilyafefelov@gmail.com>
2019-03-26 17:13:27 +0200

    Nav Complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,25 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 const Header = () => {
   return (
     <div className="section header">
-      <div className="container logo">
-        <Link to="/products" className="nav-link">
-          <img src="..\icons\logo.png" alt="minimal" href="" />
+      <div className="header-inner">
+        <div className="container logo">
+          <Link to="/products" className="nav-link">
+            <img src="..\icons\logo.png" alt="minimal" href="" />
+          </Link>
+        </div>
+        <div className="container nav">
+          <Link to="/products" className="nav-link">
+            products
         </Link>
-      </div>
-      <div className="container nav">
-        <Link to="/products" className="nav-link">
-          products
+          <div className="nav-line"></div>
+          <Link to="/services" className="nav-link">
+            services
         </Link>
-        <Link to="/services" className="nav-link">
-          services
+          <div className="nav-line"></div>
+          <Link to="/contacts" className="nav-link">
+            contacts
         </Link>
-        <Link to="/contacts" className="nav-link">
-          contacts
-        </Link>
+        </div>
       </div>
     </div>
   );
