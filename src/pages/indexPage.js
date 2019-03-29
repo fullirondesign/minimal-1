88e71ebc7b82b237243a4b93aef9915c2dfaf218
full_iron <ilyafefelov@gmail.com>
2019-03-29 15:21:34 +0200

    DONE _ ready for refactoring

--- src/pages/indexPage.js
+++ src/pages/indexPage.js
@@ -30,60 +30,63 @@ class indexPage extends Component {
     let { promotion1Flag, promotion2Flag } = this.state;
     return (
       <div>
-
-        <Slider1/>
+        <Slider1 />
 
         {/* About */}
         <div className="section aboutMinimal" id="aboutMinimal">
           <div className="text text-promo">about minimal</div>
           <div className="text text-regular">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
-            dolores, explicabo qui perspiciatis at quos inventore consequuntur
-            corrupti natus pariatur officia amet error dignissimos saepe
-            reprehenderit eum et?
-            corrupti natus pariatur officia amet error dignissimos saepe
-            reprehenderit eum et?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eligendi, dolores, explicabo qui perspiciatis at quos inventore
+            consequuntur corrupti natus pariatur officia amet error
+            dignissimos saepe reprehenderit eum et? corrupti natus pariatur
+            officia amet error dignissimos saepe reprehenderit eum et?
           </div>
           <div className="aboutMinimal-circle">
-            <img className="aboutMinimal-circle-line1" src="/img/Layer 5 copy@2x.png" />
-            <img className="aboutMinimal-circle-line2" src="/img/Layer 5 copy.png" />
-            <img className="aboutMinimal-circle-line3" src="../img/Shape 7.png" />
+            <img
+              className="aboutMinimal-circle-line1"
+              src="/img/Layer 5 copy@2x.png"
+            />
+            <img
+              className="aboutMinimal-circle-line2"
+              src="/img/Layer 5 copy.png"
+            />
+            <img
+              className="aboutMinimal-circle-line3"
+              src="../img/Shape 7.png"
+            />
             <div className="aboutMinimal-circle-img-text">
-            <div className=" aboutMinimal-circle-img-text-app">
-              App 
-            </div>
-            
-            <div className=" aboutMinimal-circle-img-text-off">
-              off the year
-            </div>
+              <div className=" aboutMinimal-circle-img-text-app">App</div>
+
+              <div className=" aboutMinimal-circle-img-text-off">
+                off the year
+              </div>
             </div>
           </div>
         </div>
-        
 
-        <Slider2/>
+        <Slider2 />
         {/* showcase 2 */}
-        
 
         {/* sale */}
         <div className="section sale">
-          <div className="promotion-container">
-            <div className="promotion-container-inner">
-              <div className="circle" />
-              <div className="circle" />
-              <div className="circle" />
-              {/* <img src="..\icons\iphone.png" alt="" /> */}
+          <div className="sale-container">
+            <div className="sale-container-inner sale-container-inner-bot-left">
+              <div className="text-sale">60%</div>
+              <div className="text-sale2">of project</div>
+              <div className="text-sale3">
+               complete</div>
             </div>
             {/* <vd /> */}
-            <div className="promotion-container-inner promotion-text">
-              promotion
+            <div className="sale-container-inner-image">
+              <img src="..\img\SVG\pinchArrows.svg"/>
             </div>
           </div>
         </div>
 
         {/* copyright */}
         <div className="section" id="copyright">
-          Copyright © 2019 CSS Author{" "}
+          Copyright © 2012 CSS Author{" "}
         </div>
       </div>
     );
