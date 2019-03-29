7220fe5f9a108e6248c5c8d960a9cb0795577b64
full_iron <ilyafefelov@gmail.com>
2019-03-29 16:07:55 +0200

    update

--- src/pages/indexPage.js
+++ src/pages/indexPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Slider1 from "../components/Slider1"
 import Slider2 from "../components/Slider2"
-// import "./App.css";
+
 
 class indexPage extends Component {
   state = {
